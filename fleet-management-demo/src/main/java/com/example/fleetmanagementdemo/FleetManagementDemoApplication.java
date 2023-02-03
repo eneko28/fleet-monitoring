73d343106c146c650d0aee0f4c9d917a8e39c298
Eneko Echeverria <eneko.echeverria-sagarna@thalesaleniaspace.com>
2023-02-03 14:54:57 +0100

    spring template

---
+++ fleet-management-demo/src/main/java/com/example/fleetmanagementdemo/FleetManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fleetmanagementdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementDemoApplication.class, args);
+	}
+
+}
